1c4816df63bddd79fe8dcd12e901bf401ea5988f
Jonathan Rockway <jon@jrock.us>
2008-11-16 01:56:59 -0600

    create necessary directories when the app starts

--- config.lisp
+++ config.lisp
@@ -4,4 +4,6 @@
 (defvar +database-directory+ #P"/home/jon/tmp/hairball-db/")
 
 (defun connect-to-database ()
+  (ensure-directories-exist +database-directory+ :verbose t)
+  (ensure-directories-exist +upload-directory+ :verbose t)
   (open-store (list :BDB +database-directory+)))
